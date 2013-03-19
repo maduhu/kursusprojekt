6dd299bb1f573a051f1b94606e40c883d02401a9
Adam Honoré <Adam.Honore@Gmail.com>
2013-03-19 15:48:21 +0100

    changed module name, trying new structure

--- magento/app/code/local/Oliver/MyModule/Block/brand.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Oliver_MyModule_Block_brand extends Mage_Core_Block_Template {
-
-    public function __construct() {
-        parent::_construct();
-        $this->setTemplate('mymodule/brand.phtml');
-    }
-}
