b94328658b236c88e7ae739b27713950dd2ccd9a
troels thomsen <tt.ttsoftware@gmail.com>
2013-05-23 12:10:41 +0200

    Some changes to the test.

--- magento/test/SortTest.php
+++ magento/test/SortTest.php
@@ -1,26 +1,40 @@
 <?php
 
+require_once("Product.php");
+require_once("Sort.php");
+
 class SortTest extends PHPUnit_Framework_TestCase {
 
-    protected $sort;
-    protected $products;
+    private $sort;
+    private $products = array();
 
-    protected function setUp() {
+    function setUp() {
+
         $this->sort = new Sort();
-        $this->products = array();
-        array_push($products, new Product('Ur', 10, mktime(0,0,0,1,1,2000)));
-        array_push($products, new Product('Ring', 20, mktime(0,0,0,1,3,2000)));
-        array_push($products, new Product('Armbånd', 40, mktime(0,0,0,1,5,2000)));
+
+        $this->products[] = new Product('Ur', 10, mktime(0, 0, 0, 1, 1, 2000));
+        $this->products[] = new Product('Ring', 20, mktime(0, 0, 0, 1, 3, 2000));
+        $this->products[] = new Product('Armbånd', 40, mktime(0, 0, 0, 1, 5, 2000));
     }
 
-    protected function tearDown() {
+    function tearDown() {
         unset($products);
         unset($sort);
     }
 
-    public function testSortByName() {
-        $actual = $this->sort->sortBy($this->products, 'name');
-        $expected = array($this->products[2],$this->products[1],$this->products[0]);
+    function testSortByName() {
+
+        $actual = $this->sort->sortBy(
+            $this->products,
+            'name'
+        );
+
+        $expected = array(
+            $this->products[2],
+            $this->products[1],
+            $this->products[0]
+        );
+
         $this->assertEquals($expected, $actual);
     }
 }
